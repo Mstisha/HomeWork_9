1fdba09394cd7cd5da0cdc1fa9dd50f39d9870eb
Slava <mistik99@mail.ru>
2023-02-13 18:28:10 +0200

    Задача 64

---
+++ Task64/Program.cs
@@ -0,0 +1,34 @@
+﻿int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не корректное число!");
+        }
+    }
+
+    return result;
+}
+
+void Rekursion(int n, int i)
+{
+    if(i <= n)
+    {
+        Console.WriteLine($"{n}");
+        n = n - 1;
+        Rekursion(n, i);
+    }
+}
+
+Console.WriteLine();
+int i = 1;
+int numbers = GetNumber("Введите натуральное число: ");
+Rekursion(numbers, i);
