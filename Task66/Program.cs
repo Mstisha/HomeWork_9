e30478c3f051e4b8115021eee8f71fa224b51901
Slava <mistik99@mail.ru>
2023-02-13 18:47:09 +0200

    Задача 66

---
+++ Task66/Program.cs
@@ -0,0 +1,35 @@
+﻿int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не корректное число!");
+        }
+    }
+
+    return result;
+}
+
+int Rekursion(int a, int b)
+{
+    int sum = 0;
+    if(a < b)
+    {
+        sum = (b + a) * (b - a + 1) / 2;
+    }
+    return sum;
+}
+
+Console.WriteLine();
+
+int number1 = GetNumber("Введите число: ");
+int number2 = GetNumber("Введите число: ");
+Console.WriteLine(Rekursion(number1, number2));
